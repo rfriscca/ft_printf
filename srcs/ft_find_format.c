e9e19b9cb6331125a524ccb8cc4c32354e6b26d2
Remi FRISCCA <rfriscca@e2r7p9.42.fr>
2016-06-16 15:49:32 +0200

    length specifier h and hh for all format

--- srcs/ft_find_format.c
+++ srcs/ft_find_format.c
@@ -6,7 +6,7 @@
 /*   By: rfriscca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/11 12:56:20 by rfriscca          #+#    #+#             */
-/*   Updated: 2016/06/16 14:41:57 by rfriscca         ###   ########.fr       */
+/*   Updated: 2016/06/16 15:48:40 by rfriscca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void		ft_no_length(char c, va_list valist, t_stock stock)
 	else if (c == 'd' || c == 'D' || c == 'i')
 		ft_printnbr(va_arg(valist, int), stock);
 	else if (c == 'c')
-		ft_printchar(va_arg(valist, int), stock);
+		ft_printchar((char)va_arg(valist, int), stock);
 	else if (c == 'u' || c == 'U')
 		ft_printunbr(va_arg(valist, unsigned int), stock);
 	else if (c == 'o' || c == 'O')
@@ -34,20 +34,49 @@ void		ft_no_length(char c, va_list valist, t_stock stock)
 		ft_printwstr(va_arg(valist, wchar_t*), stock);
 	else if (c == 'C')
 		ft_printwchar(va_arg(valist, wchar_t), stock);
+	else
+	{
+		ft_putstr("format not supportedi\n");
+		exit(1);
+	}
 }
 
 void		ft_hh_length(char c, va_list valist, t_stock stock)
 {
 	if (c == 'd' || c == 'D' || c == 'i')
-		ft_printhhnbr(va_arg(valist, int), stock);
+		ft_printnbr((char)va_arg(valist, int), stock);
 	if (c == 'u' || c == 'U')
-		ft_printhhunbr(va_arg(valist, unsigned int), stock);
+		ft_printunbr((unsigned char)va_arg(valist, unsigned int), stock);
 	if (c == 'x')
-		ft_printhhhex(va_arg(valist, int), stock);
+		ft_printhex((char)va_arg(valist, int), stock);
+	if (c == 'X')
+		ft_printhexcaps((char)va_arg(valist, int), stock);
+	if (c == 'o' || c == 'O')
+		ft_printoctal((char)va_arg(valist, int), stock);
+	else
+	{
+		ft_putstr("length specifier not supported with this format\n");
+		exit(1);
+	}
 }
 
 void		ft_h_length(char c, va_list valist, t_stock stock)
 {
+	if (c == 'd' || c == 'D' || c == 'i')
+		ft_printnbr((short int)va_arg(valist, int), stock);
+	if (c == 'u' || c == 'U')
+		ft_printunbr((short unsigned int)va_arg(valist, unsigned int), stock);
+	if (c == 'x')
+		ft_printhex((short int)va_arg(valist, int), stock);
+	if (c == 'X')
+		ft_printhexcaps((short int)va_arg(valist, int), stock);
+	if (c == 'o' || c == 'O')
+		ft_printoctal((short int)va_arg(valist, int), stock);
+	else
+	{
+		ft_putstr("length specifier not supported with this format\n");
+		exit(1);
+	}
 }
 
 void		ft_find_format(char const *restrict format, va_list valist,
